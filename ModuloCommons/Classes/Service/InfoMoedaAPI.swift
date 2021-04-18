305fd7264cf8a6c5fe9c6d1b6f2e47b8afd2fe2f
Jonattan <jonattan.sousa@gmail.com>
2021-04-18 15:13:10 -0300

    Efetuado ajustes em Struct, chamada de API para detalhes e extension para formatação de valores

--- ModuloCommons/Classes/Service/InfoMoedaAPI.swift
+++ ModuloCommons/Classes/Service/InfoMoedaAPI.swift
@@ -10,27 +10,29 @@ import Foundation
 import Alamofire
 
 
-class InfoMoedaAPI: NSObject {
+public class InfoMoedaAPI: NSObject {
     
     var moedaInfo: [MoedaInfoElement] = []
     var erros = Erros()
     
-    public func requestInfoMoedas(id: String){
-        
-        let url = "\(DadosChamadaApi.urlListaMoedas)\(id)"
-        
-        AF.request(url, method: .get, headers: DadosChamadaApi.headers).response { [self]
-            (responseData) in
-            guard let data = responseData.data else {return}
-            print(data)
-            do {
-                let retorno = try JSONDecoder().decode([MoedaInfoElement].self, from: data)
-                self.moedaInfo = retorno
-                print(retorno)
-            } catch{
-                guard let statusCode = responseData.response?.statusCode else {return}
-                self.erros.sobrescreverMensagemDeErro(statusCode: statusCode)
-            }
+    public func requestInfoMoedas(id: String, completionHandler: @escaping([MoedaInfoElement]) -> Void){ // Voltar e ver a necessidade de ser um array, creio que não
+            
+            let url = "\(DadosChamadaApi.urlListaMoedas)\(id)"
+            
+            AF.request(url, method: .get, headers: DadosChamadaApi.headers).response { [self]
+                (responseData) in
+                guard let data = responseData.data else {return}
+                print(data)
+                do {
+                    let retorno = try JSONDecoder().decode([MoedaInfoElement].self, from: data)
+                    completionHandler(retorno)
+                    print(retorno)
+                    print("--Sou Alamofire executado")
+                    
+                } catch{
+                    guard let statusCode = responseData.response?.statusCode else {return}
+                    self.erros.sobrescreverMensagemDeErro(statusCode: statusCode)
+                }
         }
     }
 }
