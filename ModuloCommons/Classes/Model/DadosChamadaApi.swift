cbc8a2b8577d1a6bdf482f9e87970dca1cecb377
Jonattan <jonattan.sousa@gmail.com>
2021-04-19 16:50:16 -0300

    Pequena correção na struct

--- ModuloCommons/Classes/Model/DadosChamadaApi.swift
+++ ModuloCommons/Classes/Model/DadosChamadaApi.swift
@@ -11,8 +11,8 @@ import Alamofire
 
 public struct DadosChamadaApi {
 //    static let urlListaMoedas = "https://rest-sandbox.coinapi.io/v1/assets/"
-    public let urlListaMoedas = "https://282f4580-2c94-4065-8554-3397e262c39f.mock.pstmn.io/primeiratela/"
-    public let headers: HTTPHeaders = [
+    public static let urlListaMoedas = "https://282f4580-2c94-4065-8554-3397e262c39f.mock.pstmn.io/primeiratela/"
+    public static let headers: HTTPHeaders = [
         "X-CoinAPI-Key": "FC08A1F5-7823-40C8-BC82-B4BE39E12796",
         "Accept": "application/json"
     ]
