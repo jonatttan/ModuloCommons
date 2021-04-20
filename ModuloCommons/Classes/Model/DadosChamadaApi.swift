49d5b80713ae95a651fedf4976644c979ef5cb66
Jonattan <jonattan.sousa@gmail.com>
2021-04-20 10:47:27 -0300

    Alterado URL API

--- ModuloCommons/Classes/Model/DadosChamadaApi.swift
+++ ModuloCommons/Classes/Model/DadosChamadaApi.swift
@@ -10,8 +10,8 @@ import UIKit
 import Alamofire
 
 public struct DadosChamadaApi {
-//    static let urlListaMoedas = "https://rest-sandbox.coinapi.io/v1/assets/"
-    public static let urlListaMoedas = "https://282f4580-2c94-4065-8554-3397e262c39f.mock.pstmn.io/primeiratela/"
+    public static let urlListaMoedas = "https://rest.coinapi.io/v1/assets/"
+//    public static let urlListaMoedas = "https://282f4580-2c94-4065-8554-3397e262c39f.mock.pstmn.io/primeiratela/"
     public static let headers: HTTPHeaders = [
         "X-CoinAPI-Key": "FC08A1F5-7823-40C8-BC82-B4BE39E12796",
         "Accept": "application/json"
